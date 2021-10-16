2debb7d62b555bf92b044eaed80bdb33893efa38
moshi <sy.41211@gmail.com>
2021-10-16 09:14:26 +0900

    Change failure case test

--- tests/test_calc.py
+++ tests/test_calc.py
@@ -3,16 +3,20 @@ from pytestdemo.calc import add, divide, multiply, sub
 
 
 def test_add():
-    assert add(6, 3) == 9
+    assert add(6, 3) == 10
+
 
 def test_sub():
     assert sub(6, 3) == 3
 
+
 def test_multiply():
     assert multiply(6, 3) == 18
 
+
 def test_divide_ok():
-    assert divide(6,3) == 2
+    assert divide(6, 3) == 2
+
 
 # 「カバレッジ = 100％」にしない場合の確認のためコメントアウト
 # def test_divide_zero_division_error():
